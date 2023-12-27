ce13aaca2dbdd662b3bfe551afec52f0022f70e2
Dillon Griffey <djgriffey@student.fullsail.edu>
2023-12-27 16:59:21 -0500

    services tab

---
+++ Ark Ascended Manager/Services/ServerUpdateService.cs
@@ -0,0 +1,228 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Text.Json;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+using System.Windows.Forms;
+
+namespace Ark_Ascended_Manager.Services
+{
+    internal class ServerUpdateService
+    {
+        private readonly string _serverConfigPath;
+
+        public ServerUpdateService(string serverConfigPath)
+        {
+            _serverConfigPath = serverConfigPath;
+        }
+
+        public async Task CheckAndUpdateServersOnStartup()
+        {
+            var servers = LoadServerProfiles();
+            foreach (var server in servers)
+            {
+                var sanitizedData = LoadSanitizedData(server.AppId);
+                if (sanitizedData == null || server.ChangeNumber != sanitizedData.ChangeNumber)
+                {
+                    await UpdateServer(server);
+                    SetChangeNumberForServer(server, sanitizedData?.ChangeNumber ?? 0);
+                }
+            }
+        }
+
+        private List<ServerConfig> LoadServerProfiles()
+        {
+            string json = File.ReadAllText(_serverConfigPath);
+            return JsonConvert.DeserializeObject<List<ServerConfig>>(json);
+
+        }
+
+        private async Task UpdateServer(ServerConfig server)
+        {
+            // Check if the ChangeNumber is set or matches the latest one from sanitized data
+            var sanitizedData = LoadSanitizedData(server.AppId);
+            if (sanitizedData == null || server.ChangeNumber != sanitizedData.ChangeNumber)
+            {
+                // Create the script for SteamCMD to update the server
+                string scriptPath = CreateSteamCMDScript(server);
+
+                // Run the SteamCMD script
+                RunSteamCMD(scriptPath);
+
+                // If needed, update the ChangeNumber after a successful update
+                SetChangeNumberForServer(server, sanitizedData?.ChangeNumber ?? 0);
+
+                // Save the updated server profile back to the servers.json file or wherever appropriate
+                SaveServerProfile(server);
+
+                // Optional: Delete the script file if you no longer need it
+                // File.Delete(scriptPath);
+            }
+        }
+        private void SaveServerProfile(ServerConfig server)
+        {
+            // Load the current list of server profiles
+            var servers = LoadServerProfiles();
+
+            // Update the specific server profile
+            var serverToUpdate = servers.FirstOrDefault(s => s.AppId == server.AppId);
+            if (serverToUpdate != null)
+            {
+                serverToUpdate.ChangeNumber = server.ChangeNumber;
+                // ... update other properties as needed
+            }
+
+            // Save the updated list back to the JSON file
+            string json = JsonConvert.SerializeObject(servers, Formatting.Indented);
+            File.WriteAllText(_serverConfigPath, json);
+        }
+        
+
+
+
+
+        private void SetChangeNumberForServer(ServerConfig server, int newChangeNumber)
+        {
+            server.ChangeNumber = newChangeNumber;
+            // Save the updated server profile back to the JSON file or wherever appropriate
+        }
+
+        private SanitizedSteamData LoadSanitizedData(string appId)
+        {
+            // Construct the path for the sanitized data file
+            string filePath = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData), "Ark Ascended Manager", appId, $"sanitizedsteamdata_{appId}.json");
+            if (File.Exists(filePath))
+            {
+                string json = File.ReadAllText(filePath);
+                return JsonConvert.DeserializeObject<SanitizedSteamData>(json);
+
+            }
+            return null;
+        }
+        private async Task UpdateServerWithSteamCMD(ServerConfig server)
+        {
+            string scriptPath = CreateSteamCMDScript(server);
+            RunSteamCMD(scriptPath);
+            // After the server is updated, you can delete the script file if needed.
+        }
+
+        private string CreateSteamCMDScript(ServerConfig server)
+        {
+            string scriptContent = @$"
+        force_install_dir ""{server.ServerPath}""
+        login anonymous
+        app_update {server.AppId} validate
+        quit
+    ";
+
+            string scriptPath = Path.Combine(Path.GetTempPath(), $"steamcmd_script_{server.AppId}.txt");
+            File.WriteAllText(scriptPath, scriptContent);
+            return scriptPath;
+        }
+        private void RunSteamCMD(string scriptPath)
+        {
+            string steamCmdPath = FindSteamCmdPath();
+            if (string.IsNullOrEmpty(steamCmdPath))
+            {
+                // Handle the error: steamcmd.exe not found
+                return;
+            }
+
+            ProcessStartInfo processStartInfo = new ProcessStartInfo
+            {
+                FileName = steamCmdPath,
+                Arguments = $"+runscript \"{scriptPath}\"",
+                UseShellExecute = true,
+                CreateNoWindow = false
+            };
+
+            using (Process process = new Process { StartInfo = processStartInfo })
+            {
+                process.Start();
+                process.WaitForExit();
+            }
+        }
+        private string FindSteamCmdPath()
+        {
+            // Define the JSON file path in the app data folder
+            string appDataFolder = Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData);
+            string appDataPath = Path.Combine(appDataFolder, "Ark Ascended Manager");
+            string jsonFilePath = Path.Combine(appDataPath, "SteamCmdPath.json");
+
+            // Check if the app data directory exists, if not, create it
+            if (!Directory.Exists(appDataPath))
+            {
+                Directory.CreateDirectory(appDataPath);
+            }
+
+            // Try to read the path from the JSON file
+            if (File.Exists(jsonFilePath))
+            {
+                string json = File.ReadAllText(jsonFilePath);
+                dynamic pathData = JsonConvert.DeserializeObject<dynamic>(json);
+                string savedPath = pathData?.SteamCmdPath;
+                if (!string.IsNullOrEmpty(savedPath) && File.Exists(savedPath))
+                {
+                    return savedPath;
+                }
+            }
+
+            // If the path is not found in the JSON file, prompt the user with OpenFileDialog
+            OpenFileDialog openFileDialog = new OpenFileDialog
+            {
+                Filter = "Executable files (*.exe)|*.exe",
+                Title = "Locate steamcmd.exe"
+            };
+
+            if (openFileDialog.ShowDialog() == DialogResult.OK)
+
+            {
+                // Save the selected path to the JSON file
+                SaveSteamCmdPath(openFileDialog.FileName, jsonFilePath);
+                return openFileDialog.FileName;
+            }
+
+            return null; // or handle this case appropriately
+        }
+        private void SaveSteamCmdPath(string path, string jsonFilePath)
+        {
+            var pathData = new { SteamCmdPath = path };
+            string json = JsonConvert.SerializeObject(pathData, Formatting.Indented);
+            File.WriteAllText(jsonFilePath, json);
+        }
+
+    }
+
+    // Assuming you have classes defined something like this:
+    public class ServerConfig
+    {
+        public string ProfileName { get; set; }
+        public string ServerStatus { get; set; }
+        public string ServerPath { get; set; }
+        public string MapName { get; set; }
+        public string AppId { get; set; }
+        public bool IsRunning { get; set; }
+        public string ServerName { get; set; }
+        public int ListenPort { get; set; }
+        public int RCONPort { get; set; }
+        public List<string> Mods { get; set; }
+        public int MaxPlayerCount { get; set; }
+        public string AdminPassword { get; set; }
+        public string ServerPassword { get; set; }
+        public bool UseBattlEye { get; set; }
+        public bool ForceRespawnDinos { get; set; }
+        public bool PreventSpawnAnimation { get; set; }
+        public int ChangeNumber { get; set; }
+        public string ChangeNumberStatus { get; set; }
+    }
+
+    public class SanitizedSteamData
+    {
+        public string AppId { get; set; }
+        public int ChangeNumber { get; set; }
+        public DateTime LastUpdated { get; set; }
+    }
+}
---
+++ Ark Ascended Manager/Services/SteamVersionControl.cs
@@ -0,0 +1,263 @@
+﻿using System;
+using System.Net.Http;
+using System.Threading;
+using System.Threading.Tasks;
+using System.IO;
+using System.Text.Json;
+using System.Text.RegularExpressions;
+using System.Collections.Generic;
+
+namespace Ark_Ascended_Manager.Services
+{
+    internal class SteamVersionControl
+    {
+        private Timer _timer;
+        private List<int> _appIds = new List<int> { 2430930 }; // Add more App IDs to this list as needed
+
+        public void StartUpdateTimer()
+        {
+            // Timer callback will call CheckForUpdates every 10 minutes
+            _timer = new Timer(async _ =>
+            {
+                await CheckForUpdates(_appIds);
+            }, null, TimeSpan.Zero, TimeSpan.FromMinutes(1));
+        }
+
+        private async Task CheckForUpdates(IEnumerable<int> appIds)
+        {
+            foreach (var appId in appIds)
+            {
+                string rawJson = await FetchAndSaveRawSteamData(appId);
+                if (!string.IsNullOrEmpty(rawJson))
+                {
+                    var match = Regex.Match(rawJson, "\"_change_number\":\\s*(\\d+)");
+                    if (match.Success)
+                    {
+                        int changeNumber = int.Parse(match.Groups[1].Value);
+                        UpdateSanitizedData(appId.ToString(), changeNumber);
+                    }
+                }
+            }
+            await UpdateServersChangeNumberStatus();
+        }
+
+        public void StopUpdateTimer()
+        {
+            _timer?.Change(Timeout.Infinite, Timeout.Infinite);
+            _timer?.Dispose();
+        }
+
+        private async Task<string> FetchAndSaveRawSteamData(int appId)
+        {
+            string content = string.Empty;
+            try
+            {
+                using var client = new HttpClient();
+                string url = $"https://api.steamcmd.net/v1/info/{appId}";
+
+                var response = await client.GetAsync(url);
+                if (response.IsSuccessStatusCode)
+                {
+                    content = await response.Content.ReadAsStringAsync();
+                    SaveContentToFile(content, $"unsanitizedsteamdata_{appId}.json", appId);
+                }
+                else
+                {
+                    Logger.Log($"Failed to fetch data for App {appId}: {response.StatusCode}");
+                }
+            }
+            catch (Exception ex)
+            {
+                Logger.Log($"Error in FetchAndSaveRawSteamData for App {appId}: {ex.Message}");
+            }
+            return content;
+        }
+
+        private void SaveContentToFile(string content, string fileName, int appId)
+        {
+            string folderPath = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData), "Ark Ascended Manager", appId.ToString());
+            Directory.CreateDirectory(folderPath); // Ensure the directory exists
+
+            string filePath = Path.Combine(folderPath, fileName);
+            try
+            {
+                File.WriteAllText(filePath, content);
+            }
+            catch (Exception ex)
+            {
+                Logger.Log($"Error saving file {fileName}: {ex.Message}");
+            }
+        }
+
+        private void UpdateSanitizedData(string appId, int newChangeNumber)
+        {
+            string baseFolderPath = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData), "Ark Ascended Manager");
+            string folderPath = Path.Combine(baseFolderPath, appId);
+            string filePath = Path.Combine(folderPath, $"sanitizedsteamdata_{appId}.json");
+
+            Directory.CreateDirectory(folderPath); // Ensure the directory exists
+
+            SanitizedSteamData currentData = new SanitizedSteamData();
+
+            if (File.Exists(filePath))
+            {
+                string json = File.ReadAllText(filePath);
+                currentData = JsonSerializer.Deserialize<SanitizedSteamData>(json) ?? new SanitizedSteamData();
+            }
+
+            currentData.AppId = appId;
+
+            if (newChangeNumber > currentData.ChangeNumber)
+            {
+                currentData.ChangeNumber = newChangeNumber;
+                currentData.LastUpdated = DateTime.UtcNow;
+
+                string json = JsonSerializer.Serialize(currentData, new JsonSerializerOptions { WriteIndented = true });
+                File.WriteAllText(filePath, json);
+
+                Logger.Log($"Updated Change Number for App {appId}: {newChangeNumber}");
+            }
+            else
+            {
+                Logger.Log($"No update required for App {appId}. Stored Change Number: {currentData.ChangeNumber}");
+            }
+        }
+        private async Task UpdateServersChangeNumberStatus()
+        {
+            // Load server configurations from the servers.json file
+            List<ServerConfig> serverConfigs = LoadServerConfigs();
+
+            // Fetch the latest change number for each App ID
+            foreach (int appId in _appIds)
+            {
+                int latestChangeNumber = await FetchLatestChangeNumber(appId);
+
+                // Compare and update each server's change number status
+                foreach (var serverConfig in serverConfigs)
+                {
+                    if (serverConfig.AppId == appId.ToString() && serverConfig.ChangeNumber != latestChangeNumber)
+                    {
+                        serverConfig.ChangeNumberStatus = "Server is Not Up to Date";
+                        // Here you would also trigger any necessary notifications or UI updates
+                    }
+                }
+            }
+
+            // Save any updates back to the servers.json file
+            SaveServerConfigs(serverConfigs);
+        }
+        private List<ServerConfig> LoadServerConfigs()
+        {
+            string appDataPath = Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData);
+            string filePath = Path.Combine(appDataPath, "Ark Ascended Manager", "servers.json");
+
+            // Check if the file exists before attempting to read
+            if (!File.Exists(filePath))
+            {
+                // Log the absence of the file or handle accordingly
+                Logger.Log($"The file {filePath} does not exist.");
+                return new List<ServerConfig>(); // Return an empty list if the file doesn't exist
+            }
+
+            try
+            {
+                // Read the file content
+                string jsonContent = File.ReadAllText(filePath);
+                // Deserialize the JSON content to a List<ServerConfig>
+                return JsonSerializer.Deserialize<List<ServerConfig>>(jsonContent) ?? new List<ServerConfig>();
+            }
+            catch (JsonException jsonEx)
+            {
+                // Log the exception or handle the deserialization error accordingly
+                Logger.Log($"Error deserializing the file {filePath}: {jsonEx.Message}");
+                return new List<ServerConfig>(); // Return an empty list in case of deserialization error
+            }
+            catch (Exception ex)
+            {
+                // Handle other exceptions that might occur
+                Logger.Log($"An error occurred while reading the file {filePath}: {ex.Message}");
+                return new List<ServerConfig>(); // Return an empty list in case of other errors
+            }
+        }
+        private void SaveServerConfigs(List<ServerConfig> serverConfigs)
+        {
+            string appDataPath = Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData);
+            string filePath = Path.Combine(appDataPath, "Ark Ascended Manager", "servers.json");
+
+            // Ensure the directory exists
+            string directoryPath = Path.GetDirectoryName(filePath);
+            if (!Directory.Exists(directoryPath))
+            {
+                Directory.CreateDirectory(directoryPath);
+            }
+
+            try
+            {
+                // Serialize the list to JSON
+                string jsonContent = JsonSerializer.Serialize(serverConfigs, new JsonSerializerOptions { WriteIndented = true });
+                // Write the JSON to the file, creating it if it does not exist or overwriting it if it does
+                File.WriteAllText(filePath, jsonContent);
+            }
+            catch (JsonException jsonEx)
+            {
+                // Log the exception or handle the serialization error accordingly
+                Logger.Log($"Error serializing server configurations: {jsonEx.Message}");
+            }
+            catch (Exception ex)
+            {
+                // Handle other exceptions that might occur
+                Logger.Log($"An error occurred while writing the server configurations to {filePath}: {ex.Message}");
+            }
+        }
+        private async Task<int> FetchLatestChangeNumber(int appId)
+        {
+            string appDataFolder = Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData);
+            string sanitizedDataFilePath = Path.Combine(appDataFolder, "Ark Ascended Manager", appId.ToString(), $"sanitizedsteamdata_{appId}.json");
+
+            try
+            {
+                // Asynchronously read the content of the file
+                string jsonData = await File.ReadAllTextAsync(sanitizedDataFilePath);
+
+                // Parse the JSON and extract the ChangeNumber
+                using var jsonDocument = JsonDocument.Parse(jsonData);
+                if (jsonDocument.RootElement.TryGetProperty("ChangeNumber", out JsonElement changeNumberElement) &&
+                    changeNumberElement.TryGetInt32(out int changeNumber))
+                {
+                    return changeNumber;
+                }
+                else
+                {
+                    // Handle the case where ChangeNumber is not found or is not an integer
+                    Logger.Log($"ChangeNumber not found or not an integer in {sanitizedDataFilePath}");
+                }
+            }
+            catch (FileNotFoundException)
+            {
+                Logger.Log($"File not found: {sanitizedDataFilePath}");
+            }
+            catch (JsonException ex)
+            {
+                Logger.Log($"JSON parsing error in file {sanitizedDataFilePath}: {ex.Message}");
+            }
+            catch (Exception ex)
+            {
+                Logger.Log($"Error reading the sanitized steam data file for App {appId}: {ex.Message}");
+            }
+
+            // If we reach this point, it means we couldn't fetch or parse the ChangeNumber
+            return -1; // Return an invalid value or throw an exception
+        }
+
+
+        public class SanitizedSteamData
+        {
+            public string AppId { get; set; }
+            public int ChangeNumber { get; set; }
+            public DateTime LastUpdated { get; set; }
+            public string ChangeNumberStatus { get; set; }
+        }
+
+        // Ensure to implement or define the Logger class according to your application's logging mechanism.
+    }
+}
